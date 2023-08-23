f2f0860664e31bde55a023f7cbf4691c5d04653b
yeop <woco11@naver.com>
2023-08-24 02:18:18 +0900

    js-3 commit

--- js/app_1.js
+++ js/app_1.js
@@ -1,7 +1,7 @@
 let age = 32;
 let userName = "Max"; //텍스트가 길어서 줄바꿈 하고싶을때 `` 사용 또는 ""+""사용한다
 let hobbies = ["Sports", "Cooking", "Reading"]; //배열값 저장공간(여러값 저장가능)
-let job = {
+let job = { //객체
   title: "Developer",
   place: "New York",
   salary: 50000,
@@ -10,17 +10,28 @@ let job = {
 let totaladultYear;
 
 function calculateAdultYears(userAge) {//매개변수 = 함수안에 투입되는 변수이름
-   return userAge - 18; //let 을 안에서 정의 해버리면 함수 안에서만 사용가능 "참조에러"뜸
-  
+   let result; //let 을 안에서 정의 해버리면 함수 안에서만 사용가능 "참조에러"뜸
+  result =userAge -18;
+  return result;
 }
 
 totaladultYear = calculateAdultYears(age);
-alert(totaladultYear);
+console.log(totaladultYear);//콘솔 정보출력하기위해 console.log 명령어 사용
 
 age = 45;
 totaladultYear = calculateAdultYears(age);
 
-alert(totaladultYear);
+console.log(totaladultYear);
 
 // let greetingText = "Hi, my name is/' Max!!!/'"; //변수 정의 let
 // alert(greetingText); //    경고창 띄우는 명령어 alert
+
+let person ={
+    name:"Max",//property=소유물
+    greet() { //객체 안에 있는 함수를 메서드라고 부른다
+        console.log("Hello!");
+    }
+};
+
+person.greet();
+
---
+++ js/operations_3.js
@@ -0,0 +1,34 @@
+//math operations = 수학 연산
+
+console.log(10 + 4); //int 정수
+console.log(10 - 4);
+console.log(10 * 4);
+console.log(10 / 4);//float 실수
+console.log(10 % 4);//나머지
+
+console.log((10 + 3 - 5) * 10);//()우선순위
+
+// let result = (10 + 3 -5) * 10;
+// result = 10 * 4;
+// result = result + 1;// == result += 1 == result++
+// result+= 5;
+// result-= 5;
+// result*= 5;
+// result/= 5;
+// console.log(result);
+
+
+
+
+console.log("Max" + " " + "Schwarzmuller");//문자열 더하기
+console.log("Max" - "ax");//빼기는 안된다
+
+
+let userName = "Max";
+console.log(userName.length);//length 는 문자열 길이를 확인가능
+console.log(userName.toUpperCase()); //toUpperCase() 는 문자열을 대문자로 표시
+
+
+let hobbies = ["Sports", "Cooking", "Reading"];
+console.log(hobbies.length);// 배열에서는 항목이 몇개인지 알려줌
+
