f2f0b4f1282cb0c83e66eed8ab27b15fb5dbd545
yeop <woco11@naver.com>
2023-08-23 21:58:54 +0900

    js-1 commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let age = 32;
+let userName = "Max"
+let hobbies = ["Sports","Cooking","Reading"];//배열값 저장공간(여러값 저장가능)
+let job ={ title:"Developer", place:"New York" , salary:50000 }; //키: 값 
+
+alert(hobbies[0]);
+
+// let greetingText = "Hi, my name is/' Max!!!/'"; //변수 정의 let
+// alert(greetingText); //    경고창 띄우는 명령어 alert
+
